fe79b5668e99106c5a0d373030846dc8e997301c
saggitus007 <edirin.obodo007@gmail.com>
2023-02-19 15:22:36 +0100

    Print alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 /**
- * main main function
- *
- * Return: always 0
- */
+* main - main function
+*
+* Return: always 0
+*/
+
 int main(void)
 {
 	char i;
 	
-	 for (i = 'a' ; i <= 'z' ; i++)
-		 putchar(i);
-	 for (i = 'A' ; i <= 'Z' ; i++)
-		 putchar(i);
-	 putchar('\n');
-		 return (0);
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	for (i = 'A' ; i <= 'Z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
 }
