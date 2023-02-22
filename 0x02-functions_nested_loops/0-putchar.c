6203e966565f4e583880d01e4a08fa5699bfdba9
saggitus007 <edirin.obodo007@gmail.com>
2023-02-22 18:10:52 +0100

    Putchar 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* main - Entry point
+* Description: 'prints _putcha to stdout'
+* Return: always o
+*/
+iint main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
