c69cc6d2dc601cde4342f58f2f16dffb1e13aacb
saggitus007 <edirin.obodo007@gmail.com>
2023-03-14 17:06:54 +0100

    Strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,7 +17,7 @@ char *_strdup(char *str)
 	
 	for (size = 0; str[size] != '\0'; size++);
 
-	ptr = (char *) malloc((size + 1) *size(char));
+	ptr = (char *) malloc((size + 1) *sizeof(char));
 
 	if (ptr == NULL)
 		return (NULL);
